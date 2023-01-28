03abc0ccc25932deb2a127a42e010bbe1f73d32b
muhammeddemirci <mhdemirci571@gmail.com>
2023-01-28 16:36:28 +0100

    7

--- PROJELER/CHECKOUT_PAGE/app.js
+++ PROJELER/CHECKOUT_PAGE/app.js
@@ -64,4 +64,16 @@ let totalAll = () => {
       2
     );
   }
-};
+};document.querySelector(".clear").addEventListener("click", () => {
+    if (confirm("Are you Sure?")) {
+      localStorage.clear();
+      document.querySelectorAll(".stotal").forEach((a) => {
+        a.innerText = "";
+      });
+  
+      document.querySelectorAll("#quantity").forEach((a) => {
+        a.innerText = "";
+      });
+    }
+  });
+  
