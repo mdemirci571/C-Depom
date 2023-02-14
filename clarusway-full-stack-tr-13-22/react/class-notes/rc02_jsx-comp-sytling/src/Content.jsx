857538e8987171d96734391bddded422b6111361
muhammeddemirci <mhdemirci571@gmail.com>
2023-02-14 19:31:41 +0100

    4

--- clarusway-full-stack-tr-13-22/react/class-notes/rc02_jsx-comp-sytling/src/Content.jsx
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc02_jsx-comp-sytling/src/Content.jsx
@@ -10,12 +10,30 @@
 //* Material UI, Styled Component, Sass, Bootstrap v.b harici kutuphaneler kullanilabilir.
 
 const Content = () => {
+  //? Local Style Object
+  const pStyle = {
+    fontFamily: "Tahoma",
+    fontSize: "1.3rem",
+    lineHeight: "1.5",
+  }
+
   return (
     <div>
-      <h2>React JS</h2>
-      <p>
+      {/* JSX */}
+      {/* Inline Styling */}
+      <h2 style={{ color: "white", backgroundColor: "red" }}>React JS</h2>
+
+      {/* Local or global variables */}
+      <p style={pStyle}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, aperiam.
       </p>
+
+      <img
+        src="https://cdn.pixabay.com/photo/2014/12/04/14/46/magnolia-trees-556718__340.jpg"
+        alt="spring-img1"
+      />
+
+      <img src="" alt="" />
     </div>
   )
 }
