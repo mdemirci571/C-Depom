ef494dc047df3cfe8b4728f4ca50b1e9a7c5be42
muhammeddemirci <mhdemirci571@gmail.com>
2023-02-18 11:54:47 +0100

    v7

--- clarusway-full-stack-tr-13-22/react/class-notes/rc05_events-useStateHook/src/components/3-useStateHook/UseStateCounter.jsx
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc05_events-useStateHook/src/components/3-useStateHook/UseStateCounter.jsx
@@ -18,10 +18,41 @@
 //?    https://reactjs.org/docs/hooks-rules.html
 //* =============================================================
 
+import { useState } from "react"
 
 const UseStateCounter = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1)
+    } else {
+      console.log("Counter can not be negative number")
+    }
+  }
+
   return (
-    <div>useStateCounter</div>
+    <div className="container text-center mt-4">
+      <h2 className="text-danger">Use State Counter</h2>
+      <h1 className="display-4">Count:{count}</h1>
+      <button onClick={increment} className="btn btn-success">
+        INC
+      </button>
+      <button onClick={() => setCount(0)} className="btn btn-danger">
+        CLR
+      </button>
+      <button onClick={decrement} className="btn btn-warning">
+        DEC
+      </button>
+
+      {/* <button onClick={() => setCount(count - 1)} className="btn btn-warning">
+        DEC
+      </button> */}
+    </div>
   )
 }
 
