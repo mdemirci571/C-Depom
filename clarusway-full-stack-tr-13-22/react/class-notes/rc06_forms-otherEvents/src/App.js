c3f249a1f35356d37a825c970fc9e039079eaaf3
muhammeddemirci <mhdemirci571@gmail.com>
2023-02-20 19:50:28 +0100

    v5

--- clarusway-full-stack-tr-13-22/react/class-notes/rc06_forms-otherEvents/src/App.js
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc06_forms-otherEvents/src/App.js
@@ -1,15 +1,15 @@
 // import Form from "./components/1-forms/Form"
-import FormWithObject from "./components/1-forms/FormWithObject"
-// import KeyboardEvents from "./components/2-keyboardClipboardEvents/KeyboardClipboard"
+// import FormWithObject from "./components/1-forms/FormWithObject"
+import KeyboardEvents from "./components/2-keyboardClipboardEvents/KeyboardClipboard"
 // import MouseEvent from "./components/3-mouseEvents/MouseEvent"
 
 function App() {
   return (
     <div className="container mt-4 text-center">
       {/* <Form /> */}
-      <FormWithObject />
-      {/* <KeyboardEvents />
-      <MouseEvent /> */}
+      {/* <FormWithObject /> */}
+      <KeyboardEvents />
+      {/* <MouseEvent />  */}
     </div>
   )
 }
