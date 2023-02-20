3d7c3fed15eb5340597f27cb24d8684e3543f7d9
muhammeddemirci <mhdemirci571@gmail.com>
2023-02-20 18:49:32 +0100

    v2

--- clarusway-full-stack-tr-13-22/react/class-notes/rc06_forms-otherEvents/src/components/1-forms/Form.jsx
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc06_forms-otherEvents/src/components/1-forms/Form.jsx
@@ -1,33 +1,57 @@
+import { useState } from "react"
+
 const Form = () => {
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleUsername = (e) => {
+    // console.log(e.target.value)
+    setUsername(e.target.value)
+  }
   return (
     <form>
       <h2 className="display-6 text-danger">FORMS</h2>
 
       <div className="mb-3">
         <label htmlFor="username" className="form-label">
-          Username
+          Username: <span>{username}</span>
         </label>
-        <input type="email" className="form-control" id="username" />
+        <input
+          type="text"
+          className="form-control"
+          id="username"
+          onChange={handleUsername}
+          value={username}
+        />
       </div>
 
       <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          Email address
+        <label htmlFor="email" className="form-label">
+          Email
         </label>
-        <input type="email" className="form-control" id="exampleInputEmail1" />
+        <input
+          type="email"
+          className="form-control"
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
       </div>
       <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">
+        <label htmlFor="password" className="form-label">
           Password
         </label>
         <input
           type="password"
           className="form-control"
-          id="exampleInputPassword1"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
         />
       </div>
 
-      <button type="submit" className="btn btn-primary">
+      <button type="submit" className="btn btn-danger">
         Submit
       </button>
     </form>
