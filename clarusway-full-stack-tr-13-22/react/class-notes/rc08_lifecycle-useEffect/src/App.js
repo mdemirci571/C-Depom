43171e5a9c99a5f6ee1af7ac33ee7c2c46f4c1bd
muhammeddemirci <mhdemirci571@gmail.com>
2023-02-22 20:06:41 +0100

    v3

--- clarusway-full-stack-tr-13-22/react/class-notes/rc08_lifecycle-useEffect/src/App.js
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc08_lifecycle-useEffect/src/App.js
@@ -1,9 +1,20 @@
+import { useState } from "react"
 import LifeCycleMethods from "./components/LifeCycleMethods"
+import UseEffectHook from "./components/UseEffectHook"
 
 function App() {
+  const [toggle, setToggle] = useState(false)
+
   return (
     <div className="container text-center mt-4">
-      <LifeCycleMethods />
+      <button className="btn btn-danger" onClick={() => setToggle(!toggle)}>
+        {toggle ? "Hide" : "Show"}
+      </button>
+
+      {/* {toggle ? <LifeCycleMethods /> : null} */}
+      {/* {toggle && <LifeCycleMethods />} */}
+
+      {toggle && <UseEffectHook />}
     </div>
   )
 }
