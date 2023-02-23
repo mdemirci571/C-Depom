bb0a78cbeba5bc43d6302f43300c1f75fdb972f7
muhammeddemirci <mhdemirci571@gmail.com>
2023-02-23 19:04:02 +0100

    v1

--- clarusway-full-stack-tr-13-22/react/class-notes/rc09_useeffect-axios-example/tutorial-app-inClass/src/pages/Home.jsx
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc09_useeffect-axios-example/tutorial-app-inClass/src/pages/Home.jsx
@@ -1,13 +1,23 @@
-import AddTutorial from "../components/AddTutorial";
-import TutorialList from "../components/TutorialList";
+import axios from "axios"
+import AddTutorial from "../components/AddTutorial"
+import TutorialList from "../components/TutorialList"
 
 const Home = () => {
+  const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/"
+
+  const getTutorials = async () => {
+    const { data } = await axios(BASE_URL)
+    console.log(data)
+  }
+
+  getTutorials()
+
   return (
     <>
       <AddTutorial />
       <TutorialList />
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
