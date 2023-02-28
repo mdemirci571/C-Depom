72989f3c5843de6e311d0390a0c70daf78135669
muhammeddemirci <mhdemirci571@gmail.com>
2023-02-28 20:41:46 +0100

    nav

--- clarusway-full-stack-tr-13-22/react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/App.js
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/App.js
@@ -3,6 +3,7 @@ import Nav from "./components/Nav"
 import Contact from "./pages/Contact"
 import Home from "./pages/Home"
 import People from "./pages/People"
+import PersonDetail from "./pages/PersonDetail"
 import NotFound from "./pages/NotFound"
 import { Routes, Route } from "react-router-dom"
 import Paths from "./pages/Paths"
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/people" element={<People />} />
+        <Route path="/people/:id" element={<PersonDetail />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/paths" element={<Paths />} />
         <Route path="*" element={<NotFound />} />
--- clarusway-full-stack-tr-13-22/react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/People.jsx
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/People.jsx
@@ -1,40 +1,47 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
+import { Link, Navigate, useNavigate } from "react-router-dom"
 
 const People = () => {
-  const [people, setPeople] = useState([]);
+  const [people, setPeople] = useState([])
+  const navigate = useNavigate()
 
   const getPeople = () => {
     fetch("https://reqres.in/api/users")
       .then((res) => res.json())
       .then((data) => setPeople(data.data))
-      .catch((err) => console.log(err));
-  };
+      .catch((err) => console.log(err))
+  }
   useEffect(() => {
-    getPeople();
-  }, []);
+    getPeople()
+  }, [])
 
   return (
     <div className="container text-center mt-4">
       <h1>PEOPLE LIST</h1>
       <div className="row justify-content-center g-3">
         {people?.map((person) => {
-          const { id, first_name, last_name, avatar } = person;
+          const { id, first_name, last_name, avatar } = person
           return (
             <div
               key={id}
               className=" col-sm-12 col-md-6 col-lg-4"
               type="button"
+              //? Absolute path (tam adres vermek)
+              onClick={() => navigate(`/people/${id}`)}
             >
               <img className="rounded" src={avatar} alt="img" />
               <h6>
                 {first_name} {last_name}
               </h6>
+
+              {/* <Link to={`/people/${id}`}> Deneme</Link>
+              <Navigate to={`/people/${id}`} /> */}
             </div>
-          );
+          )
         })}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default People;
+export default People
