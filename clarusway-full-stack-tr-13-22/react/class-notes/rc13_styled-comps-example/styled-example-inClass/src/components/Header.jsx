874bf39517d1438b3829d02ba54a2b2d9181f419
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-02 20:54:55 +0100

    2

--- clarusway-full-stack-tr-13-22/react/class-notes/rc13_styled-comps-example/styled-example-inClass/src/components/Header.jsx
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc13_styled-comps-example/styled-example-inClass/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import Button from "./styled/Button"
+import Flex from "./styled/Flex"
 import Nav from "./styled/Nav"
 
 const Header = () => {
@@ -14,6 +15,18 @@ const Header = () => {
           <Button bg="#AF3A53">Talk To Adviser</Button>
         </div>
       </Nav>
+
+      <Flex>
+        <div>
+          <h1>The IT Career of Your Dreams Starts Here!</h1>
+          <p>
+            Clarusway is a leading international software Bootcamp. Join a micro
+            class online with other trainees and learn coding skills with a
+            highly-skilled instructor.
+          </p>
+          <Button bg="#AF3A53">Start your New Carier</Button>
+        </div>
+      </Flex>
     </div>
   )
 }
