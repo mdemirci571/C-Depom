cc69170dc35e7bd674f752fcc5c89f93e191110c
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-02 19:51:50 +0100

    1

--- clarusway-full-stack-tr-13-22/react/class-notes/rc13_styled-comp-intro/styled-intro_inClass/src/App.js
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc13_styled-comp-intro/styled-intro_inClass/src/App.js
@@ -1,19 +1,27 @@
 import { Button, FS13Button } from "./components/Buton.styled"
 import HeaderText from "./components/HeaderText"
+import Links from "./components/Links"
 import Sarmal from "./components/Sarmal"
 
 const App = () => {
   return (
-    <Sarmal bg="#bebe">
-      <HeaderText renk="hotpink">Styled Components </HeaderText>
-      <HeaderText renk="#734586" bgRenk="#eee">
-        Styled Components
-      </HeaderText>
-      <Button>Send</Button>
-      <Button primary>Send</Button>
-      <FS13Button react>CALIS</FS13Button>
-      <FS13Button>DINLEN</FS13Button>
-    </Sarmal>
+    <>
+      <Sarmal bg="#bebe">
+        <HeaderText renk="hotpink">Styled Components </HeaderText>
+        <HeaderText renk="#734586" bgRenk="#eee">
+          Styled Components
+        </HeaderText>
+        <Button>Send</Button>
+        <Button primary>Send</Button>
+        <FS13Button react>CALIS</FS13Button>
+        <FS13Button>DINLEN</FS13Button>
+      </Sarmal>
+      <Sarmal>
+        <Links href="https://www.clarusway.com" target="_blank">
+          Clarusway Web Site
+        </Links>
+      </Sarmal>
+    </>
   )
 }
 
