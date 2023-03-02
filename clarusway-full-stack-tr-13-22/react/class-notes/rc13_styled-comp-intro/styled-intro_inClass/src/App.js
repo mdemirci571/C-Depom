522c47940bba9a633e0f0acd0adbb823f1dac080
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-02 19:49:27 +0100

    1

--- clarusway-full-stack-tr-13-22/react/class-notes/rc13_styled-comp-intro/styled-intro_inClass/src/App.js
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc13_styled-comp-intro/styled-intro_inClass/src/App.js
@@ -1,14 +1,19 @@
 import { Button, FS13Button } from "./components/Buton.styled"
+import HeaderText from "./components/HeaderText"
+import Sarmal from "./components/Sarmal"
 
 const App = () => {
   return (
-    <>
-      <h1>Styled Components</h1>
+    <Sarmal bg="#bebe">
+      <HeaderText renk="hotpink">Styled Components </HeaderText>
+      <HeaderText renk="#734586" bgRenk="#eee">
+        Styled Components
+      </HeaderText>
       <Button>Send</Button>
       <Button primary>Send</Button>
       <FS13Button react>CALIS</FS13Button>
       <FS13Button>DINLEN</FS13Button>
-    </>
+    </Sarmal>
   )
 }
 
