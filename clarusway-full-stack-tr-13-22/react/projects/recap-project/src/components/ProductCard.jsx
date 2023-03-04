c7dd0e30ff9199b0b87c4c5d04c4738d5c7feb2d
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-04 11:53:56 +0100

    1

--- clarusway-full-stack-tr-13-22/react/projects/recap-project/src/components/ProductCard.jsx
+++ clarusway-full-stack-tr-13-22/react/projects/recap-project/src/components/ProductCard.jsx
@@ -1,7 +1,22 @@
+import axios from "axios";
 import React from "react";
 
-const ProductCard = ({item}) => {
-  const {name,image,price,dampingRate,amount,id} = item;
+const ProductCard = ({ item, getProducts }) => {
+  const url = process.env.REACT_APP_API_URL;
+  const { name, image, price, dampingRate, amount, id } = item;
+
+  const handleMinus = () => {};
+
+  const handlePlus = () => {};
+
+  const handleRemove = async () => {
+    try {
+      await axios.delete(`${url}/${id}`);
+    } catch (error) {
+      console.log(error);
+    }
+    getProducts();
+  };
 
   return (
     <div className="card shadow-lg mb-3">
@@ -32,19 +47,25 @@ const ProductCard = ({item}) => {
             </div>
             <div className="border border-1 border-dark shadow-lg d-flex justify-content-center p-2">
               <div className="quantity-controller">
-                <button className="btn btn-secondary btn-sm">
+                <button
+                  className="btn btn-secondary btn-sm"
+                  onClick={handleMinus}>
                   <i className="fas fa-minus"></i>
                 </button>
                 <p className="d-inline mx-4" id="product-quantity">
                   {amount}
                 </p>
-                <button className="btn btn-secondary btn-sm">
+                <button
+                  className="btn btn-secondary btn-sm"
+                  onClick={handlePlus}>
                   <i className="fas fa-plus"></i>
                 </button>
               </div>
             </div>
             <div className="product-removal mt-4">
-              <button className="btn btn-danger btn-sm w-100 remove-product">
+              <button
+                className="btn btn-danger btn-sm w-100 remove-product"
+                onClick={handleRemove}>
                 <i className="fa-solid fa-trash-can me-2"></i>Remove
               </button>
             </div>
