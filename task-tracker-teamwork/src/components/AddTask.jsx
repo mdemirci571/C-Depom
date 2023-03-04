95a8a7fbf49b0e1906ed8b55ed5c94c0f7bc565c
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-04 15:18:31 +0100

    1

--- task-tracker-teamwork/src/components/AddTask.jsx
+++ task-tracker-teamwork/src/components/AddTask.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-
 const AddTask = () => {
   return (
-    <div>AddTask</div>
-  )
-}
+    <div>
+      <form className="form-group">
 
-export default AddTask
+        <div className="form-group text-start">
+          <label htmlFor="formGroupExampleInput">Add Task</label>
+          <input
+            type="text"
+            className="form-control"
+            id="addTask"
+            placeholder="Add Task"
+          />
+        </div>
+        <div className="form-group text-start">
+          <label htmlFor="formGroupExampleInput2">Day & Time</label>
+          <input
+            type="text"
+            className="form-control"
+            id="dayTime"
+            placeholder="Add Day & Time"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-info mt-3 w-100">
+          Save Task
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddTask;
