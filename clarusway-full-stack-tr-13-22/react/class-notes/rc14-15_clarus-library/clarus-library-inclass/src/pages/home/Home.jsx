b71310f7159f60629cab3c0b58634d3e5c55c596
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-08 18:30:00 +0100

    1

--- clarusway-full-stack-tr-13-22/react/class-notes/rc14-15_clarus-library/clarus-library-inclass/src/pages/home/Home.jsx
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc14-15_clarus-library/clarus-library-inclass/src/pages/home/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import { HomeContainer } from './Home.style'
+import { HomeContainer, HomeImg, MainContainer } from './Home.style'
+import Header from '../../components/header/Header'
+import homeImg from "../../assets/books.jpg";
 
 const Home = () => {
   return (
     <HomeContainer>
-      
+      <Header/>
+
+      <HomeImg>
+        <img src={homeImg} alt="home image" />
+      </HomeImg>
+
+      <MainContainer>
+
+        
+
+      </MainContainer>
 
     </HomeContainer>
   )
--- clarusway-full-stack-tr-13-22/react/class-notes/rc14-15_clarus-library/clarus-library-inclass/src/pages/home/Home.style.jsx
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc14-15_clarus-library/clarus-library-inclass/src/pages/home/Home.style.jsx
@@ -5,10 +5,16 @@ export const HomeContainer = styled(Flex)`
     flex-direction:column;
 `
 export const HomeImg = styled.div`
-    width: 35%;
-    max-height: 50vh;
+    margin: 2rem;
+    display:flex;
+    justify-content:center;
+    width:100%;
+  img {
+    width: 45%;
+    height: 50vh;
     border-radius: 1rem;
-`
+  }
+`;
 export const MainContainer = styled(Flex)`
     flex-direction: row;
 `
