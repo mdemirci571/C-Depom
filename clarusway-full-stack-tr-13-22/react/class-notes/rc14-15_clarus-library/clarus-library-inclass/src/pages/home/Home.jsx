b974aa542791cd5d4e196eeb9da286444d06b323
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-08 19:05:07 +0100

    1

--- clarusway-full-stack-tr-13-22/react/class-notes/rc14-15_clarus-library/clarus-library-inclass/src/pages/home/Home.jsx
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc14-15_clarus-library/clarus-library-inclass/src/pages/home/Home.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { HomeContainer, HomeImg, MainContainer } from './Home.style'
 import Header from '../../components/header/Header'
 import homeImg from "../../assets/books.jpg";
 
 const Home = () => {
+  // const printType = ["all","books","magazines"]
+  const [query,setQuery] = useState("")
+  const [selectType,setSelectType] = useState("all")
+  const [myData,setMyData] = useState([])
+
+
+
   return (
     <HomeContainer>
-      <Header/>
+      <Header setQuery={setQuery} setSelectType={setSelectType} />
 
       <HomeImg>
         <img src={homeImg} alt="home image" />
@@ -14,7 +21,7 @@ const Home = () => {
 
       <MainContainer>
 
-        
+
 
       </MainContainer>
 
