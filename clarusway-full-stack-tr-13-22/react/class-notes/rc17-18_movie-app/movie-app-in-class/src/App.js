7240e0b21247ab49a8fa4a233f7a717565a6a953
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-13 18:50:22 +0100

    v6

--- clarusway-full-stack-tr-13-22/react/class-notes/rc17-18_movie-app/movie-app-in-class/src/App.js
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc17-18_movie-app/movie-app-in-class/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Register from "./pages/Register";
+import AppRouter from "./router/AppRouter";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Register />
+      <AppRouter />
     </div>
   );
 };
