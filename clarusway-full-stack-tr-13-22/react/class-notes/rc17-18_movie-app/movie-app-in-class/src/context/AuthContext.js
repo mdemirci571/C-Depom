db456992c8c4a62dc7514df374d812212df25b23
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-13 20:28:32 +0100

    v8

--- clarusway-full-stack-tr-13-22/react/class-notes/rc17-18_movie-app/movie-app-in-class/src/context/AuthContext.js
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc17-18_movie-app/movie-app-in-class/src/context/AuthContext.js
@@ -1,4 +1,6 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { createContext } from "react";
+import { auth } from "../auth/firebase";
 
 // export const {Provider} = createContext()
 export const AuthContext = createContext();
@@ -9,6 +11,21 @@ export const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
   const values = { currentUser: { displayName: "felix franko" } };
+
+  const createUser = async (email, password) => {
+    try {
+      //? yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
+      let userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(userCredential);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
 
