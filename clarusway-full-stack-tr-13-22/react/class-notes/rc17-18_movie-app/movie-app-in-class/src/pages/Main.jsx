311e079e68a4feb14998e06bd692aba07829d2d1
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-14 19:59:24 +0100

    v8

--- clarusway-full-stack-tr-13-22/react/class-notes/rc17-18_movie-app/movie-app-in-class/src/pages/Main.jsx
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc17-18_movie-app/movie-app-in-class/src/pages/Main.jsx
@@ -3,13 +3,20 @@ import MovieCard from "../components/MovieCard";
 import { MovieContext } from "../context/MovieContext";
 
 const Main = () => {
-  const { movies } = useContext(MovieContext);
+  const { movies, loading } = useContext(MovieContext);
   return (
     <div>
       <div className="flex justify-center flex-wrap">
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} {...movie} />
-        ))}
+        {loading ? (
+          <div
+            className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-600 mt-52"
+            role="status"
+          >
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        ) : (
+          movies.map((movie) => <MovieCard key={movie.id} {...movie} />)
+        )}
       </div>
     </div>
   );
