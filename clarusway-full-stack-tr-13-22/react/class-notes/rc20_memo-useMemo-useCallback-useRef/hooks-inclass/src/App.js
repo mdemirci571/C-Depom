ac0394733051fb0562fbb5880dd5b398eb9e6e73
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-18 11:30:06 +0100

    v15

--- clarusway-full-stack-tr-13-22/react/class-notes/rc20_memo-useMemo-useCallback-useRef/hooks-inclass/src/App.js
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc20_memo-useMemo-useCallback-useRef/hooks-inclass/src/App.js
@@ -1,18 +1,47 @@
-import { useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import Header from "./components/Header";
 import HeaderMemo from "./components/HeaderMemo";
 import TaxComp from "./components/TaxComp";
 import UseRefComp from "./components/UseRefComp";
+import axios from "axios";
+import Card from "./components/Card";
 
 function App() {
   const [count, setCount] = useState(0);
   const [user,setUser] = useState("anthony")
   const [taxDataState, setTaxDataState] = useState({ taxRate: 0.18, ship: 25 });//ilk oluşumda referansını tanımlıyor ve o referans sabit kalıyor
+  const [data,setData] = useState([])
+  const [search,setSearch] = useState("")
 
   const taxData = {taxRate:0.18,ship:25};//her render bellekteki refereansı değşiyor
 
   const taxData1 = useRef({ taxRate: 0.18, ship: 25 });
   console.log(taxData1)//hafızada referansı sabit ama kendisi değişebilir olan verileri tanımlamak istiyorsak
+
+
+  const getData = async () => {
+    const { data } = await axios.get("https://dummyjson.com/products");
+    // console.log(data)
+    setData(data.products)
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
+  
+  const handleSearch = (e) =>{
+    setSearch(e.target.value)
+  
+  }
+
+  const filteredData = data.filter(product=> product.title.toLowerCase().includes(search.toLowerCase()))
+
+  const filteredMemoData = useMemo(()=> {
+    console.log("useMemo çalıştı")
+    return data.filter((product) =>
+      product.title.toLowerCase().includes(search.toLowerCase())
+    );
+  },[data,search])//refereansı sabitlemiş oluyor ve hesapmlanın sonucunu belleğe alıyor
 
   return (
     <div className="container mt-2">
@@ -39,10 +68,16 @@ function App() {
       <hr />
       {/* <TaxComp taxData={taxData} /> */}
       {/* <TaxComp taxData={taxDataState} /> */}
-      <TaxComp taxData={taxData1} />
+      {/* <TaxComp taxData={taxData1} /> */}
 
-      <hr/>
-      <UseRefComp />
+      <hr />
+      {/* <UseRefComp /> */}
+      <hr />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <input type="text" value={search} onChange={handleSearch} />
+      </div>
+      {/* <Card data={filteredData} /> */}
+      <Card data={filteredMemoData} />
     </div>
   );
 }
