9ef86e6ef2586ef1cfc2e3865061da04f900ac17
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-18 10:09:22 +0100

    v11

--- clarusway-full-stack-tr-13-22/react/class-notes/rc20_memo-useMemo-useCallback-useRef/hooks-inclass/src/components/UseRefComp.jsx
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc20_memo-useMemo-useCallback-useRef/hooks-inclass/src/components/UseRefComp.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const UseRefComp = () => {
-  
+  console.log("Render => useRefComp")
+  const [text,setText] = useState("")
+  const inputRef = useRef()
+
+  const handleSearch = () =>{
+    console.log(text)
+    console.log(inputRef.current)
+    console.log(inputRef)
+    console.log(inputRef.current.value);
+    setText(inputRef.current.value);
+  }
   return (
     <div>
       UseRefComp
       <div style={{ display: "flex", justifyContent: "center" }}>
-        <input type="text" />
-        <button type="button">
+        {/* <input
+          type="text"
+          onChange={(e) => setText(e.target.value)}
+        /> */}
+        <input
+          type="text"
+          ref={inputRef}
+        />
+        <button type="button" onClick={handleSearch}>
           Search
         </button>
       </div>
