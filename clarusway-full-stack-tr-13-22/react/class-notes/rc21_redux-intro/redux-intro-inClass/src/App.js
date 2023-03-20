583874fb62f55b1517c25b897c8c8e0566502b02
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-20 20:27:44 +0100

    10

--- clarusway-full-stack-tr-13-22/react/class-notes/rc21_redux-intro/redux-intro-inClass/src/App.js
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc21_redux-intro/redux-intro-inClass/src/App.js
@@ -1,14 +1,23 @@
-import "./App.css";
-import Counter from "./components/counter/Counter";
-import Todo from "./components/todo/Todo";
+import "./App.css"
+import Counter from "./components/counter/Counter"
+import Todo from "./components/todo/Todo"
+import { legacy_createStore as createStore } from "redux"
+import { Provider } from "react-redux"
+import reducer from "./redux"
 
 function App() {
+  //? Store olusturduk.
+  const store = createStore(reducer)
+
   return (
     <div className="app">
-      <Counter />
-      <Todo />
+      {/* Store ile tum uygulamayi sarmaladik */}
+      <Provider store={store}>
+        <Counter />
+        <Todo />
+      </Provider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
