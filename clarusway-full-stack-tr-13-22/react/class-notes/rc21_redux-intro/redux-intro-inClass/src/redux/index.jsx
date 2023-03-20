596c038a3847fceaaf4721f3f94cd24c0b087f82
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-20 20:27:19 +0100

    9

--- clarusway-full-stack-tr-13-22/react/class-notes/rc21_redux-intro/redux-intro-inClass/src/redux/index.jsx
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc21_redux-intro/redux-intro-inClass/src/redux/index.jsx
@@ -1,13 +1,20 @@
-const initialState = {
-  counter: 0, //? Global state
-}
 
-export const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "INCREASE":
-      return { counter: state.counter + 1 }
+// const initialState = {
+//   counter: 0, //? Global state
+// }
 
-    default:
-      break
-  }
-}
+// //? Pure js function
+// //? Bir dis bagimlilik olmamali
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "INCREASE":
+//       return { counter: state.counter + 1 }
+//     case "DECREASE":
+//       return { counter: state.counter - 1 }
+//     case "RESET":
+//       return { counter: 0 }
+//     default:
+//       return state
+//   }
+// }
+// export default reducer
