baf68ee13f2ef207b8c95629cec268d7a85b3dfd
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-27 23:03:24 +0200

    v2

--- clarusway-full-stack-tr-13-22/react/class-notes/rc23-28_stock-app/stock-app-inClass/src/pages/Firms.jsx
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc23-28_stock-app/stock-app-inClass/src/pages/Firms.jsx
@@ -1,8 +1,9 @@
 import { Button, Grid } from "@mui/material"
 import Typography from "@mui/material/Typography"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import FirmCard from "../components/FirmCard"
+import FirmModal from "../components/modals/FirmModal"
 import useStockCall from "../hooks/useStockCall"
 import { flex } from "../styles/globalStyle"
 
@@ -31,6 +32,17 @@ const Firms = () => {
 
   const { getStockData } = useStockCall()
   const { firms } = useSelector((state) => state.stock)
+  const [open, setOpen] = useState(false)
+
+  const [info, setInfo] = useState({
+    name: "",
+    phone: "",
+    address: "",
+    image: "",
+  })
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
 
   useEffect(() => {
     // getFirms()
@@ -45,12 +57,20 @@ const Firms = () => {
         Firm
       </Typography>
 
-      <Button variant="contained">New Firm</Button>
+      <Button variant="contained" onClick={handleOpen}>
+        New Firm
+      </Button>
 
+      <FirmModal
+        open={open}
+        handleClose={handleClose}
+        info={info}
+        setInfo={setInfo}
+      />
       <Grid container sx={flex}>
         {firms?.map((firm) => (
           <Grid item key={firm.id}>
-            <FirmCard firm={firm} />
+            <FirmCard firm={firm} setOpen={setOpen} setInfo={setInfo} />
           </Grid>
         ))}
       </Grid>
