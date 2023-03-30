da695b7751b50df12b14657b8efdd7ee50f379e2
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-30 22:49:35 +0200

    v15

--- clarusway-full-stack-tr-13-22/react/class-notes/rc23-28_stock-app/stock-app-inClass/src/pages/Home.jsx
+++ clarusway-full-stack-tr-13-22/react/class-notes/rc23-28_stock-app/stock-app-inClass/src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import { Typography } from "@mui/material"
+import { useEffect } from "react"
 import Charts from "../components/Charts"
 import KpiCards from "../components/KpiCards"
+import useStockCall from "../hooks/useStockCall"
 
 const Home = () => {
+  const { getStockData } = useStockCall()
+
+  useEffect(() => {
+    getStockData("sales")
+    getStockData("purchases")
+  }, [])
+
   return (
     <div>
       <Typography variant="h4" color="error" mb={3}>
