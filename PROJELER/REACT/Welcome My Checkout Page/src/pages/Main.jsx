8efa545b6f64808fb86cac82c1207d2fe310bd7a
mdemirci571 <mhdemirci571@gmail.com>
2023-04-23 20:17:23 +0200

    4

---
+++ PROJELER/REACT/Welcome My Checkout Page/src/pages/Main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import AllProductList from "./AllProductList";
+
+
+
+
+const Main = () => {
+
+  return (
+    <div className="container">
+      <h1 className="text-center mt-5 text-danger">Welcome My Checkout Page</h1>
+      <AllProductList/>
+
+    </div>
+  );
+};
+
+export default Main;
