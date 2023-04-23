ea26e991e8a153c52fe94564d4d04d7e5eae244a
mdemirci571 <mhdemirci571@gmail.com>
2023-04-23 03:56:55 +0200

    5

---
+++ PROJELER/REACT/Welcome My Checkout Page/src/data.jsx
@@ -0,0 +1,48 @@
+const data= [
+  {
+   name: "Samsung Galaxy S21 Ultra",
+   avatar: "https://cdn.mos.cms.futurecdn.net/3FEaW8KvZrF9XUAbut6M2j.jpg",
+   id: "1",
+   image: "https://images.samsung.com/no/smartphones/galaxy-s21/buy/02_ImageCarousel/01_FamilyShot/S21_Family_KV_PC_img.jpg",
+   price: 1199.99,
+   dampingRate: 0.8,
+   amount: 1
+  },
+  {
+   name: "Apple iPhone 13 Pro",
+   avatar: "https://www.apple.com/v/iphone-13-pro/c/images/overview/hero__zkpqjtz96qom_large.jpg",
+   id: "2",
+   image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MM2Y3?wid=1000&hei=1000&fmt=jpeg&qlt=95&.v=1630507733000",
+   price: 999.99,
+   dampingRate: 0.9,
+   amount: 1
+  },
+  {
+   name: "Google Pixel 6 Pro",
+   avatar: "https://cdn.vox-cdn.com/thumbor/6Q_Hv_R16TwbILFwJmc8T0WgCvA=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22753114/google-pixel-6-pro-hardware-image-1.jpg",
+   id: "3",
+   image: "https://www.androidauthority.com/wp-content/uploads/2021/10/Google-Pixel-6-Pro-angled-camera-closeup-on-gray-floor.jpg",
+   price: 899.99,
+   dampingRate: 0.7,
+   amount: 1
+  },
+  {
+   name: "OnePlus 9 Pro",
+   avatar: "https://www.oneplus.com/launch/assets/images/home/pro/hero-1200.jpg",
+   id: "4",
+   image: "https://shared.cdn.smp.schibsted.com/v2/images/8b9a7345-85fb-4f2f-9ce7-7dacb58aa735?fit=crop&format=auto&h=1125&w=2000&s=3c7947c3e49288bf1a297ed942d77e2947040d7e",
+   price: 999.99,
+   dampingRate: 0.6,
+   amount: 1
+  },
+  {
+   name: "Xiaomi Mi 11",
+   avatar: "https://m.gsmarena.com/vv/newsimg/20/12/xiaomi-mi-11-renders/-728/gsmarena_000.jpg",
+   id: "5",
+   image: "https://shared.cdn.smp.schibsted.com/v2/images/655ca664-a6bb-4e57-bb4b-74d8237fd04c?fit=crop&format=auto&h=2250&w=4000&s=dcc44cc80c5d8ff65c756824a3e6c89fdd40e180",
+   price: 699.99,
+   dampingRate: 0.5,
+   amount: 1
+  }
+ ];
+ export default data;
